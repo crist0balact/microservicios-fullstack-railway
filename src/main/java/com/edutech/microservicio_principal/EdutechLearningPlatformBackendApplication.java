4a2af6f1ddcc463113ab994802e6f4e488c8f0fa
MiauUwUMuuuuu <diego.donoso.montero03@gmail.com>
2025-05-21 16:17:12 -0400

    Initial commit: Estructura base del proyecto Spring Boot

---
+++ src/main/java/com/edutech/microservicio_principal/EdutechLearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.microservicio_principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechLearningPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechLearningPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech/microservicio_principal/EdutechLearningPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.microservicio_principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechLearningPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
