832481e8dfb794131cfd950bf927f98a6d413be6
MiauUwUMuuuuu <diego.donoso.montero03@gmail.com>
2025-05-22 01:01:56 -0400

    Corregidos nombres de clases y métodos: CursoController, CursoRepository y entidad Curso

--- src/main/java/com/edutech/microservicio_principal/CursoController.java
+++ src/main/java/com/edutech/microservicio_principal/CursoController.java
@@ -1,22 +1,29 @@
 package com.edutech.microservicio_principal;
+package com.edutech.microservice_principal;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-import jakarta.persistence.*;
 
 @RestController
 @RequestMapping("/api/cursos")
-public class CursoController {
+public class CursoController { 
+
+    @Autowired
+    private CursoRepository cursoRepository;
 
     @GetMapping
-    public List<Curso> getAllCursos() {
+    public List<Curso> getAllCursos() { 
+        return cursoRepository.findAll();
     }
 
     @PostMapping
-    public Curso createCurso(@RequestBody Curso curso) {
+    public Curso createCurso(@RequestBody Curso curso) { 
+        return cursoRepository.save(curso);
     }
 
     @GetMapping("/{id}")
     public Curso getCursoById(@PathVariable Long id) {
+        return cursoRepository.findById(id).orElse(null);
     }
 }
---
+++ src/main/java/com/edutech/microservicio_principal/CursoRepository.java
@@ -0,0 +1,8 @@
+package com.edutech.microservicio_principal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
