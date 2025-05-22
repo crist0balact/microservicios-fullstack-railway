7d68751be4303193972ee7ca71a0f9c6879ccc1e
MiauUwUMuuuuu <diego.donoso.montero03@gmail.com>
2025-05-22 12:08:35 -0400

    Actualizacion java por parte de cris.

--- src/main/java/com/edutech/microservicio_principal/Curso.java
+++ src/main/java/com/edutech/microservicio_principal/Curso.java
@@ -1,6 +1,13 @@
 package com.edutech.microservicio_principal;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Curso {
@@ -8,15 +15,29 @@ public class Curso {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    @Column(nullable = false)
     private String nombre;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(nullable = false)
     private String descripcion;
+
+    @Min(0)
+    @Column(nullable = false)
+    private int valorCurso;
 
     public Curso() {
     }
 
-    public Curso(String nombre, String descripcion) {
+    public Curso(Long id, String nombre, String descripcion, int valorCurso) {
+        this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.valorCurso = valorCurso;
     }
 
     public Long getId() {
@@ -43,12 +64,21 @@ public class Curso {
         this.descripcion = descripcion;
     }
 
+    public int getValorCurso() {
+        return valorCurso;
+    }
+
+    public void setValorCurso(int valorCurso) {
+        this.valorCurso = valorCurso;
+    }
+
     @Override
     public String toString() {
         return "Curso{" +
                "id=" + id +
                ", nombre='" + nombre + '\'' +
                ", descripcion='" + descripcion + '\'' +
+               ", valor=" + valorCurso +
                '}';
     }
 }
