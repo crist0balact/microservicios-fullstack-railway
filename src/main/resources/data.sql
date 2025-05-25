10c12cf021450faaa88ce4dece3f38bc04ffbdd1
crist0balact <cr.cisternast@duocuc.cl>
2025-05-24 20:54:11 -0400

    Modificación de propiedades o application.properties antes configurado para H2, para la conexión mediante MySQL. Eliminación de los datos default creados para la tabla Curso de la base de datos, eliminación de carpeta innecesarias(resources\bin)

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO CURSO (nombre, descripcion, valor_curso) 
-VALUES 
-    ('Spring Boot Básico', 'Curso introductorio', 19990),
-    ('Microservicios Avanzados', 'Arquitectura distribuida', 29990);
